654590b32bf42e40a948c18f48f8692fd899cedb
Felipe Bona <felipechiodinibona@hotmail.com>
2023-03-04 16:02:01 -0300

    making controller address

--- app/Http/Controllers/StoreAddressController.php
+++ app/Http/Controllers/StoreAddressController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\StoreAddress;
+use App\Models\UserStore;
 use Illuminate\Http\Request;
 
 class StoreAddressController extends Controller
 {
 
-    public function store(Request $request)
+    public function insert(UserStore $userStore, Request $request)
     {
-        StoreAddress::updateOrCreate($request->only('number, street', 'city', 'state'));
+        StoreAddress::updateOrCreate([
+            'user_store_id' => $userStore->id,
+        ], [
+            'street' => $request->street,
+            'number' => $request->number,
+            'district' => $request->district,
+            'city' => $request->city,
+            'state' => $request->state
+        ]);
+
+        return response()->json(['message' => 'Endereço salvo']);
     }
 
 }
